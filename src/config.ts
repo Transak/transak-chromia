d6ff5b3326797210535d0c4d9361575762d1ad1f
Sachin Thakur <sachin@transak.com>
2024-10-29 18:12:03 +0530

    fix(ENG-10388): fixed transaction link

--- src/config.ts
+++ src/config.ts
@@ -2,13 +2,13 @@ import { Network } from './types';
 
 export const networks: Record<string, Network> = {
   main: {
-    transactionLink: signature => `https://explorer.chromia.com/mainnet/transaction/${signature}`,
+    transactionLink: (signature, blockchainRid) => `https://explorer.chromia.com/mainnet/transaction/${blockchainRid}/${signature}`,
     walletLink: address => `https://explorer.chromia.com/mainnet/wallet/${address}`,
     networkUrl: 'https://acala-rpc.dwellir.com',
     networkName: 'mainnet',
   },
   testnet: {
-    transactionLink: signature => `https://explorer.chromia.com/testnet/transaction/${signature}`,
+    transactionLink: (signature, blockchainRid) => `https://explorer.chromia.com/testnet/transaction/${blockchainRid}/${signature}`,
     walletLink: address => `https://explorer.chromia.com/testnet/wallet/${address}`,
     networkName: 'testnet',
     networkUrl: "https://node0.testnet.chromia.com:7740/"
